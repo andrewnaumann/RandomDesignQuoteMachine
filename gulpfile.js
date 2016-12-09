fd8931701a21c0716510ef9f5548bad5ede32b1f
Andrew Naumann <apnaumann@gmail.com>
2016-12-09 14:56:59 -0500

    refactor to work without foundation

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,23 @@
 var gulp = require('gulp');
-var $    = require('gulp-load-plugins')();
+var sass = require('gulp-sass');
+var bs = require('browser-sync').create();
 
-var sassPaths = [
-  'bower_components/foundation-sites/scss',
-  'bower_components/motion-ui/src'
-];
-
-gulp.task('sass', function() {
-  return gulp.src('scss/app.scss')
-    .pipe($.sass({
-      includePaths: sassPaths
-    })
-      .on('error', $.sass.logError))
-    .pipe($.autoprefixer({
-      browsers: ['last 2 versions', 'ie >= 9']
-    }))
-    .pipe(gulp.dest('css'));
+gulp.task('browser-sync', ['sass'], function() {
+    bs.init({
+        server: {
+            baseDir: "./"
+        }
+    });
 });
 
-gulp.task('default', ['sass'], function() {
-  gulp.watch(['scss/**/*.scss'], ['sass']);
+gulp.task('sass', function () {
+    return gulp.src('scss/*.scss')
+                .pipe(sass().on('error', sass.logError))
+                .pipe(gulp.dest('css'))
+                .pipe(bs.reload({stream: true}));
+});
+
+gulp.task('watch', ['browser-sync'], function () {
+    gulp.watch("scss/*.scss", ['sass']);
+    gulp.watch("*.html").on('change', bs.reload);
 });
